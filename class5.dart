6299b9e14c14f27c5f67f21f5859e51169a710f3
UroojWali <Urooj5435@gmail.com>
2024-03-10 14:47:09 +0500

    github lecture

---
+++ class5.dart
@@ -0,0 +1,18 @@
+void main (){
+  int maths = 60;
+  int urdu = 70;
+  int english = 80 ;
+ int obtained = maths + urdu + english ;
+ int totalmarks = 300 ;
+ num percentage  = (obtained / totalmarks) * 100 ;  
+ print (percentage );  
+  if ( percentage  >= 90 ) { 
+    print("A+");
+   }
+   else if ( percentage>= 50 && percentage < 60 ){
+    print ("D");
+   }
+   else  if ( percentage >= 60){ 
+    print ("C");
+   }
+  }

63e2d437e5a3147a230d8cb100e6f0f1f11c4514
UroojWali <Urooj5435@gmail.com>
2024-03-10 16:06:08 +0500

    utook

---
+++ 3.0.dart
@@ -0,0 +1,13 @@
+void main (){ 
+  List < dynamic > mylist = [ 30 ,40 ,50, 60, 70, "urooj"];
+    List<dynamic>all list  = [ 80 , 60, 50 ,40 , 70 , " urooj "];
+    mylist [ 4]=10;// singleelement update
+    mylist.replace
+
+
+
+
+  print(mylist);
+
+  
+   } 
